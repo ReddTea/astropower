0523b605cac7a7686f92f561acc1a7f0d1d21b42
Felipe Castillo <felipe.castillo.t@ing.uchile.cl>
2016-12-25 21:03:15 -0300

    trainmodel without data

--- trainmodel.py
+++ trainmodel.py
@@ -1,5 +1,11 @@
 from keras.models import Sequential
-from keras.layers import Dense, Convolution2D, pooling
+from keras.layers import MaxoutDense, Convolution2D, pooling, MaxPooling2D
+
+# train data
+x_train = # imagenes fits
+y_train = # respuestas
+
+#
 
 model = Sequential()
 model.add(Convolution2D(16,10,10,border_mode='same'),input_shape=(207,207))
@@ -18,4 +24,13 @@ model.add(Convolution2D(256,3,3,border_mode='same'),input_shape=(207,207))
 #6x6
 
 model.add(Convolution2D(256,3,3,border_mode='same'),input_shape=(207,207))#4x4
-model.add(pooling.MaxPooling2D(pool_size=(2,2),border_mode='same')) #2x2   
+model.add(MaxPooling2D(pool_size=(2,2),border_mode='same')) #2x2
+
+model.add(MaxoutDense(2048))
+
+model.compile(loss='categorical_crossentropy', optimizer='sgd')
+
+model.fit(x_train, y_train, nb_epoch=1)
+
+from sklearn.externals import joblib
+joblib.dump(model,"model.pkl")
