4fa6712b52730ab03187100eeddd0334e5dd0b15
Felipe Castillo <felipe.castillo.t@ing.uchile.cl>
2016-12-25 21:49:21 -0300

    FirstFullVer

--- trainmodel.py
+++ trainmodel.py
@@ -1,11 +1,36 @@
 from keras.models import Sequential
 from keras.layers import MaxoutDense, Convolution2D, pooling, MaxPooling2D
+import pandas as pd
+import scipy as sp
+import glob
+from astropy.io import fits
 
 # train data
-x_train = # imagenes fits
-y_train = # respuestas
+GZ2 = pd.read_csv('GalaxyZoo2.csv')
+r1 = GZ2["t01_smooth_or_features_a01_smooth_debiased"]
+r2 = GZ2["t01_smooth_or_features_a02_features_or_disk_debiased"]
+r3 = GZ2["t01_smooth_or_features_a03_star_or_artifact_debiased"]
+y_train = []
+for i in range(len(r1)):
+    maxprob = max(r1[i],r2[i],r3[i])
+    if r1[i]==maxprob :
+        y_train[i] = (r1>0.33)*1
+    elif r1[i]==maxprob:
+        y_train[i] = (r2>0.33)*2
+    elif r1[i]==maxprob:
+        y_train[i] =  (r3>0.33)*3
+y_train = np.array(y_train)
 
-#
+files =  glob.glob("./fits/*.fits")
+x_train = np.array([])
+for pathfile in files:
+    hdulist = fits.open(pathfile)
+    data = hdulist[0].data
+    x_train = np.array((x_train, data))
+x_train = np.delete(x_train,0)
+
+
+
 
 model = Sequential()
 model.add(Convolution2D(16,10,10,border_mode='same'),input_shape=(207,207))
